11c5e80d2ea862e1b16e0c1bfb33c8ff8d6ab5d9
Joshua-Tihabwangye <tihabwangyej@gmail.com>
2025-12-15 21:48:03 +0300

    Updated the layout of the system

--- src/pages/services/tours/TourBookingCreatePage.tsx
+++ src/pages/services/tours/TourBookingCreatePage.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { toastManager } from "../../../utils/toastManager";
 
 export default function TourBookingCreatePage() {
